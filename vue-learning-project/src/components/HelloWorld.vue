28b78814f68e62cf10741401c287d3febfceeb00
locb65 <locb65@gmail.com>
2023-04-29 11:53:43 -0400

    added a simple HelloWorld template and rendered it to the APP UI

--- vue-learning-project/src/components/HelloWorld.vue
+++ vue-learning-project/src/components/HelloWorld.vue
@@ -1,43 +1,9 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
+<!-- Vue uses template based components unlike reactjs function or class base components -->
+<!-- Vue Js template components is kind of like a built in ejs file -->
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <!-- html div -->
+  <div>
+      <h1>Hello World</h1>
   </div>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
